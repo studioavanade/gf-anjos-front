194b2dc0bae6772ef109cbb7042fe64dff604acf
Marcelino Borges <marcelino.botelho.ext@dasa.com.br>
2021-12-28 15:50:49 -0300

    icons and directories for the payment-steps

--- src/assets/img/index.ts
+++ src/assets/img/index.ts
@@ -1,14 +1,30 @@
-
 // TopNavigation
-import LOGO_FACEBOOK from '../img/top-navigation/logo_facebook.svg';
-import LOGO_GFALCOES from '../img/top-navigation/logo_gfalcoes.svg';
-import LOGO_INSTAGRAM from '../img/top-navigation/logo_instagram.svg';
-import LOGO_LINKEDIN from '../img/top-navigation/logo_linkedin.svg';
-import LOGO_LOGIN from '../img/top-navigation/logo_login.svg';
-import LOGO_YOUTUBE from '../img/top-navigation/logo_youtube.svg';
+import LOGO_FACEBOOK from "../img/top-navigation/logo_facebook.svg";
+import LOGO_GFALCOES from "../img/top-navigation/logo_gfalcoes.svg";
+import LOGO_INSTAGRAM from "../img/top-navigation/logo_instagram.svg";
+import LOGO_LINKEDIN from "../img/top-navigation/logo_linkedin.svg";
+import LOGO_LOGIN from "../img/top-navigation/logo_login.svg";
+import LOGO_YOUTUBE from "../img/top-navigation/logo_youtube.svg";
 
+//Icons payment steps
+import ICON_CONFIRMATION_STEP from "../img/payment/icon-confirmation-step.svg";
+import ICON_IDENTITY_STEP from "../img/payment/icon-identity-step.svg";
+import ICON_PAYMENT_STEP from "../img/payment/icon-payment-step.svg";
+import ICON_REGISTRATION_STEP from "../img/payment/icon-registration-step.svg";
 
 // Donation Cards
-import DONATION_CARDS from '../img/donation-cards/donation-cards.svg';
+import DONATION_CARDS from "../img/donation-cards/donation-cards.svg";
 
-export { LOGO_FACEBOOK, LOGO_GFALCOES, LOGO_INSTAGRAM, LOGO_LINKEDIN, LOGO_LOGIN, LOGO_YOUTUBE, DONATION_CARDS}
+export {
+  LOGO_FACEBOOK,
+  LOGO_GFALCOES,
+  LOGO_INSTAGRAM,
+  LOGO_LINKEDIN,
+  LOGO_LOGIN,
+  LOGO_YOUTUBE,
+  DONATION_CARDS,
+  ICON_CONFIRMATION_STEP,
+  ICON_IDENTITY_STEP,
+  ICON_PAYMENT_STEP,
+  ICON_REGISTRATION_STEP,
+};

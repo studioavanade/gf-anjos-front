4d1dcdd43e8997a17d240a26ec768fb0cc488c25
caio1882 <caiooliver182@gmail.com>
2021-12-28 09:56:43 -0300

    conflict correction

--- src/modules/pages/home/index.tsx
+++ src/modules/pages/home/index.tsx
@@ -1,11 +1,7 @@
-import Email from "../ambassador-register/email/email";
-import PhotoUpload from "../ambassador-register/photo-upload/photo-upload";
-
 const Home = () => {
   return <>
-   {/* <Email /> */}
-   <PhotoUpload />
-  </>
+  Home
+  </>;
 };
 
 export default Home;

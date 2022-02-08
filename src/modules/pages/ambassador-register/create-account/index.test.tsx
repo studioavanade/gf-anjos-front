6ad7396163b8c0ad0e8fb8646b297e81bce3fb6e
caio1882 <caiooliver182@gmail.com>
2022-02-08 11:36:42 -0300

    Test create account component

--- src/modules/pages/ambassador-register/create-account/index.test.tsx
+++ src/modules/pages/ambassador-register/create-account/index.test.tsx
@@ -1,26 +1,20 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render} from "@testing-library/react";
 import AmbassadorCreateAccount from "./index";
-
-import { Provider } from "react-redux";
-import store from "../../../../store/index";
-import { BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ROUTING_PATHS from "../../../../routes/paths";
 
 describe("Ambassador Create Account Page", () => {
   it("Shoul render Ambassador Create", () => {
-     render(
-      <Provider store={store}>
-        <BrowserRouter>
-          <Routes>
-            <Route
-              path={ROUTING_PATHS.AmbassadorCreateAccount}
-              element={<AmbassadorCreateAccount />}
-            />
-          </Routes>
-        </BrowserRouter>
-      </Provider>
+    render(
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path={ROUTING_PATHS.AmbassadorCreateAccount}
+            element={<AmbassadorCreateAccount />}
+          />
+        </Routes>
+      </BrowserRouter>
     );
-
   });
 });

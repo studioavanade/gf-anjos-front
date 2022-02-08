30f542d201a19fe502bfd27a465532016643e006
caio1882 <caiooliver182@gmail.com>
2022-02-08 11:24:40 -0300

    test personal information component

--- src/modules/pages/ambassador-register/personal-information/index.test.tsx
+++ src/modules/pages/ambassador-register/personal-information/index.test.tsx
@@ -2,10 +2,20 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import PersonalInformation from "./index";
 
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import ROUTING_PATHS from "../../../../routes/paths";
+
 describe('Personal Information Page', () => {
     it('Should render on Personal Information screen', () => {
-        render(<PersonalInformation />);
-
-        expect(screen.getByText('Faça seu cadastro')).toBeInTheDocument();
+        render(
+            <BrowserRouter>
+              <Routes>
+                <Route
+                  path={ROUTING_PATHS.AmbassadorCreateAccount}
+                  element={<PersonalInformation />}
+                />
+              </Routes>
+            </BrowserRouter>
+          );
     })
 })

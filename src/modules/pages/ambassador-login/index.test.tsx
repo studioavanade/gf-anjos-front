97e1e4a2a8b4ea631ac5dbb3d01676a434ebdf36
caio1882 <caiooliver182@gmail.com>
2022-02-09 15:03:48 -0300

    Test Ambassador Login correction

--- src/modules/pages/ambassador-login/index.test.tsx
+++ src/modules/pages/ambassador-login/index.test.tsx
@@ -1,20 +1,30 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AmbassadorLogin from "./index";
-import ROUTING_PATHS from "../../../routes/paths/index";
+import { Provider } from "react-redux";
+import store from "../../../store";
 
 describe("Ambassador Login Page", () => {
   it("Should render Ambassador Login Screen", () => {
-    render(
+    const { getByTestId } = render(
+      <Provider store={store}>
       <BrowserRouter>
         <Routes>
           <Route
-            path={ROUTING_PATHS.AmbassadorLogin}
+            path={"/"}
             element={<AmbassadorLogin />}
           />
         </Routes>
       </BrowserRouter>
+      </Provider>
     );
+
+    const buttonSaved = getByTestId("button-continued");
+    expect(buttonSaved).toBeInTheDocument();
+
+    expect(screen.getByText("LOGIN")).toBeInTheDocument();
+    expect(screen.getByText("Entrar")).toBeInTheDocument();
+    expect(screen.getByText("Criar conta")).toBeInTheDocument();
   });
 });
